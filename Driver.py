a873300a8577640abdf28380e796bc2cedfebb0d
Naitik Shah <nrshah11@asu.edu>
2018-07-19 00:20:45 -0700

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -1,8 +1,8 @@
 from AutoScaler import *
 import time
 
-_ami = 'ami-626b7a02' # ami for app-tier-auto Naitik's
-_req_queue_url = 'https://sqs.us-west-1.amazonaws.com/050256975407/image-request'
+_ami = '****' # ami for app-tier-auto Naitik's
+_req_queue_url = 'https://sqs.us-west-1.amazonaws.com/***/image-request'
 _sleep_time = 2.0
 
 scaler = AutoScaler(ami=_ami, \
